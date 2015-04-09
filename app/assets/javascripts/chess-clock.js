c8a40369518fce4c62988ed7c1f74298b2674acc
tgoldenberg <thomasagoldenberg@gmail.com>
2015-04-09 13:02:05 -0400

    styled notation bar and timer

--- app/assets/javascripts/chess-clock.js
+++ app/assets/javascripts/chess-clock.js
@@ -1,8 +1,8 @@
 (function() {
   jQuery(function($) {
     var addedTimePerMove, formatTime, initialTime, intervalId, pad, remainingTime, switchTurn, turn, update;
-    initialTime = 3 * 60;
-    addedTimePerMove = 2;
+    initialTime = 15 * 60;
+    addedTimePerMove = 0;
     turn = 0;
     remainingTime = [initialTime, initialTime];
     pad = function(x) {
@@ -38,16 +38,16 @@
         return remainingTime[turn] += addedTimePerMove;
       }
     };
-    $('body').mousedown(function(event) {
-      switchTurn();
-      return false;
-    });
-    $('body').keydown(function(event) {
-      if (event.keyCode === 32) {
-        switchTurn();
-        return false;
-      }
-    });
+    // $('body').mousedown(function(event) {
+    //   switchTurn();
+    //   return false;
+    // });
+    // $('body').keydown(function(event) {
+    //   if (event.keyCode === 32) {
+    //     switchTurn();
+    //     return false;
+    //   }
+    // });
     $('#time0').addClass('turn');
     return intervalId = setInterval(update, 1000);
   });
